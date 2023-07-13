4ca668d582586316db6ab92e4ba5e2bb61814de7
shivam sharma <shivamsharma2695@gmail.com>
2023-07-13 14:30:47 +0530

    Update and rename autokube.sh to master_kube.sh
    changed the script and renamed the file also

---
+++ master_kube.sh
@@ -0,0 +1,23 @@
+sudo su
+apt update -y
+apt install docker.io -y
+
+systemctl start docker
+systemctl enable docker
+
+curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
+echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
+
+apt update -y
+apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+
+sudo su
+kubeadm init
+
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+kubeadm token create --print-join-command
